4f0eb03ea81e00ef0ccd201bb5abc27fc6b2eb28
alan20062006 <alan20062006@vip.qq.com>
2017-03-23 23:16:04 +0800

    First Working Little Spider

--- SpiderMain.py
+++ SpiderMain.py
@@ -1,20 +1,7 @@
-from urllib import request as urllib2      # urllib2
+from GetAll import getinfo
 
-from pyquery import PyQuery
+urlbase='https://biometry.nci.nih.gov'
+location='./'
 
-url='https://biometry.nci.nih.gov/cdas/datasets/idata/'
-
-with urllib2.urlopen(url) as f:
-    data = f.read()
-    print('Status:', f.status, f.reason)
-    for k, v in f.getheaders():
-        print('%s: %s' % (k, v))
-    print('Data:', data.decode('utf-8'))
-    data  = data.decode('utf-8')
-    p = PyQuery(data)
-    content = p('#maincontent').eq(0).find('li')
-    for i in range(len(content)):
-        print(content.eq(i).find('a').eq(0).text())
-        print(content.eq(i).find('a').eq(0).attr('href'))
-
+getinfo('./',urlbase+'/cdas/datasets/idata/')
 
